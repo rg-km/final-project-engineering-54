3bd21c19f5ebae3d08a44d06441689fa6fde7c61
Aditya Rizqi Ardhana <ardhanajames777@gmail.com>
2022-06-08 20:40:46 +0700

    docs: update config tailwind, add utils.

---
+++ frontend/src/utlis/Utils.jsx
@@ -0,0 +1,3 @@
+export const classes = (withCondition, ignoreCondition) => {
+    return `${withCondition} ${ignoreCondition}`;
+}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,24 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue-code': '#28237A', 
+        'gray-code': '#BBBBEF', 
+        'orange-code': '#FD9727', 
+        'indigo-code': '#656EE3', 
+        'purple-code': '#3A39B4', 
+      },
+      screens: {
+        '6xs': {'max': '240px'},
+        '5xs': {'max': '320px'},
+        '4xs': {'max': '375px'},
+        '3xs': {'max': '411px'},
+        '2xs': {'max': '480px'},
+        '1xs': {'max': '540px'},
+        'xs': {'max': '640px'},
+      }
+    }
   },
   plugins: [],
 }
