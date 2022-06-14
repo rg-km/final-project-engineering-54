a222b5d2f1df49f271169af1038e2026d9c60635
Damara Syaidil <damaraccd11@gmail.com>
2022-06-14 14:31:34 +0700

    feat: openning database and starting the server

---
+++ backend/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"database/sql"
+
+	"github.com/rg-km/final-project-engineering-54/backend/source"
+	"github.com/rg-km/final-project-engineering-54/backend/api"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// create func main for openning the database and starting the server
+func main() {
+	db, err := sql.Open("sqlite3", "backend/db/codeswer.db")
+	if err != nil {
+		panic(err)
+	}
+
+	users := source.NewUsersSource(db)
+
+	mainApi := api.NewAPI(*users)
+	mainApi.Start()
+}
