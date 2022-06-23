26341ec449932e2077e3766e178068ba1cfbf4b2
Aditya Rizqi Ardhana <ardhanajames777@gmail.com>
2022-06-23 17:20:08 +0700

    docs: fix little mistake

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -19,8 +19,8 @@ export default function NotFound() {
             <article className="notfound-component">
                 <div id="container_notfound" className="flex flex-col justify-center items-center text-center space-y-16 py-10">
                     <img src="/asset/img/404.webp" alt="404" width={400}></img>
-                    <h1 className="text-gray-800 font-medium poppins text-[1.15rem]">Tetap tenang. Coba cek kembali alamat URL yang kamu masukkan atau kembali ke 
-                        <Link to="/" className="text-blue-code font-semibold hover:text-orange-code leading-normal"> Halaman utama</Link>
+                    <h1 className="text-gray-800 font-medium poppins text-[1.15rem] leading-normal">Tetap tenang. Coba cek kembali alamat URL yang kamu masukkan atau kembali ke 
+                        <Link to="/" className="text-blue-code font-semibold hover:text-orange-code"> Halaman utama</Link>
                     </h1>
                 </div>
 
